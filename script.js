8993d6c5176bdf75ea5e8c6a64c70e34e33a7e7d
Louis Gitonga <louisgitonga19@gmail.com>
2026-04-13 06:22:41 +0300

    js

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-const products = [
-    {
-        name: "Black Hoodie",
-        price: 2500,
-        image: "images/hoodie.jpg"
-    },
-    {
-        name: "White Tee",
-        price: 1500,
-        image: "images/tshirt.jpg"
-    },
-    {
-        name: "Street Jacket",
-        price: 4000,
-        image: "images/jacket.jpg"
-    }
-];
-
 const container = document.getElementById("product-container");
 
 async function getProducts() {
     try {
-        const res = await fetch("https://fakestoreapi.com/products");
+        const res = await fetch("https://dummyjson.com/products");
         const data = await res.json();
 
-        displayProducts(data);
+        displayProducts(data.products);
 
     } catch (error) {
         console.log("Error fetching products:", error);
     }
+} 
+
+function displayProducts(products) {
+
+    products.forEach(product => {
+
+        const card = document.createElement("div");
+        card.classList.add("product");
+
+        
+        const productName = product.title;
+        const productPrice = product.price;
+        const productImage = product.thumbnail;
+        const productCategory = product.category;
+
+        card.innerHTML = `
+            <img src="${productImage}" alt="${productName}">
+            <h3>${productName}</h3>
+            <p>${productCategory}</p>
+            <p>KES ${(productPrice * 130).toFixed(0)}</p>
+        `;
+
+        container.appendChild(card);
+    });
 }
