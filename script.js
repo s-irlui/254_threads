7f45ab54ef1b3e4a8e79e72e05e5c2bd36e1dbd5
Louis Gitonga <louisgitonga19@gmail.com>
2026-04-13 06:05:03 +0300

    js dom

---
+++ script.js
@@ -0,0 +1,19 @@
+const products = [
+    {
+        name: "Black Hoodie",
+        price: 2500,
+        image: "images/hoodie.jpg"
+    },
+    {
+        name: "White Tee",
+        price: 1500,
+        image: "images/tshirt.jpg"
+    },
+    {
+        name: "Street Jacket",
+        price: 4000,
+        image: "images/jacket.jpg"
+    }
+];
+
+const container = document.getElementById("product-container");
