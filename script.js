1e91b380188293c9f090d676c729bc89d3ad7dc9
Louis Gitonga <louisgitonga19@gmail.com>
2026-04-13 06:27:15 +0300

    js

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
+const products = [
+    {
+        name: "254 Noir Hoodie",
+        price: 3500,
+        image: "images/hoodie.jpg",
+        category: "Street Luxe"
+    },
+    {
+        name: "Mtaa Drift Tee",
+        price: 1800,
+        image: "images/tshirt.jpg",
+        category: "Urban Drop"
+    },
+    {
+        name: "Nairobi Shadow Jacket",
+        price: 5000,
+        image: "images/jacket.jpg",
+        category: "Premium Street"
+    },
+    {
+        name: "254 Essentials Cap",
+        price: 1200,
+        image: "images/cap.jpg",
+        category: "Accessories"
+    }
+];
+
 const container = document.getElementById("product-container");
 
-async function getProducts() {
-    try {
-        const res = await fetch("https://dummyjson.com/products");
-        const data = await res.json();
+function displayProducts() {
 
-        displayProducts(data.products);
-
-    } catch (error) {
-        console.log("Error fetching products:", error);
-    }
-} 
-
-function displayProducts(products) {
+    container.innerHTML = ""; // reset
 
     products.forEach(product => {
 
         const card = document.createElement("div");
         card.classList.add("product");
 
-        
-        const productName = product.title;
-        const productPrice = product.price;
-        const productImage = product.thumbnail;
-        const productCategory = product.category;
-
         card.innerHTML = `
-            <img src="${productImage}" alt="${productName}">
-            <h3>${productName}</h3>
-            <p>${productCategory}</p>
-            <p>KES ${(productPrice * 130).toFixed(0)}</p>
+            <img src="${product.image}" alt="${product.name}">
+            <h3>${product.name}</h3>
+            <p>${product.category}</p>
+            <p>KES ${product.price}</p>
         `;
 
         container.appendChild(card);
     });
 }
+
+displayProducts();
